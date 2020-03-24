a3c8d7df34139e7dfd61a38f832bbd61eb64f701
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-03-24 13:38:57 -0700

    deleted:app.js

--- Raphael project/weekly-assignment-4/src/components/App.jsx
+++ Raphael project/weekly-assignment-4/src/components/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import MemeComponent from "./MemeComponent";
-import EditComponent from "./EditComponent";
 
 const MEME_END_POINT = "https://api.imgflip.com/get_memes";
 
