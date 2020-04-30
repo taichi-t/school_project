329f3ab7e3fca122c1bfc4af460b4527c1af61a1
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-04-30 00:42:02 -0700

    install:material-ui

--- Raphael project/midterm-project_hooks/src/components/Dashbord.jsx
+++ Raphael project/midterm-project_hooks/src/components/Dashbord.jsx
@@ -1,8 +1,31 @@
 import React, { useContext } from "react";
 import { Context } from "../contexts/Context";
 
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
 export const Dashbord = () => {
   const { data } = useContext(Context);
-  console.log(data);
-  return <div>hello</div>;
+
+  const city = data && data.name;
+  const currentweather = data && data.weather[0].main;
+  const feelsLike = data && data.main["feels_like"];
+  console.log(data && data.name);
+  return (
+    <Card>
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {city}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {currentweather}
+        </Typography>
+        <Typography color="textSecondary">{feelsLike}&#08451;</Typography>
+        {/* <Typography variant="body2" component="p">
+          {state.isNotFound ? <p>Not Found</p> : null}
+        </Typography> */}
+      </CardContent>
+    </Card>
+  );
 };
--- Raphael project/midterm-project_hooks/src/contexts/Context.js
+++ Raphael project/midterm-project_hooks/src/contexts/Context.js
@@ -6,39 +6,40 @@ require("dotenv").config();
 const apiKey = process.env.REACT_APP_APIKEY;
 
 export const ContextProvier = (props) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
   const [isNotFound, setCondition] = useState(false);
 
   useEffect(() => {
-    const fetchData = (url) => {
-      fetch(url).then((response) => {
-        if (response.status === 404) {
-          console.log("city is not founded " + response.status);
-          setCondition({ isNotFound: true });
-          return;
-        }
-
-        if (response.status !== 200) {
-          console.log(
-            "Looks like there was a problem. Status Code:" + response.status
-          );
-          return;
-        }
-
-        response
-          .json()
-          .then((fetchData) => {
-            setData([...data, fetchData]);
-          })
-          .catch((error) => {
-            console.log("this is a error" + error);
-          });
-      });
-    };
     fetchData(
       `http://api.openweathermap.org/data/2.5/weather?q=vancouver&units=metric&appid=${apiKey}`
     );
   }, []);
+
+  const fetchData = (url) => {
+    fetch(url).then((response) => {
+      if (response.status === 404) {
+        console.log("city is not founded " + response.status);
+        setCondition({ isNotFound: true });
+        return;
+      }
+
+      if (response.status !== 200) {
+        console.log(
+          "Looks like there was a problem. Status Code:" + response.status
+        );
+        return;
+      }
+
+      response
+        .json()
+        .then((fetchData) => {
+          setData(fetchData);
+        })
+        .catch((error) => {
+          console.log("this is a error" + error);
+        });
+    });
+  };
 
   return (
     <Context.Provider value={{ data, isNotFound }}>
