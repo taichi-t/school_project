01742306e025a46dd8277afa2d289e509ebb4082
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-04 01:50:41 -0700

    add:style

--- Raphael project/weekly-assignment-5/src/components/History.jsx
+++ Raphael project/weekly-assignment-5/src/components/History.jsx
@@ -1,21 +1,47 @@
 import React, { useContext } from "react";
 import { BookContext } from "../contexts/BookContext";
 
+//style
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+
 export const History = () => {
   const { state } = useContext(BookContext);
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      color: "black",
+    },
+  }));
+
+  const classes = useStyles();
+
   const item =
     state.history.length === 0 ? (
-      <p>There is no history</p>
+      <>
+        <p style={{ color: "black" }}>There is no history</p>
+        <Button color="primary">
+          <Link to="/">Back</Link>
+        </Button>
+      </>
     ) : (
-      <ul>
-        {state.history.map((item) => (
-          <li key={item.id}>
-            title:{item.title}, author:{item.author}, finished at{" "}
-            {item.finishedAt}
-          </li>
-        ))}
-      </ul>
+      <>
+        <List className={classes.root}>
+          {state.history.map((item) => (
+            <ListItem key={item.id}>
+              <ListItemText primary={item.title} secondary={item.finishedAt} />
+              {/* title:{item.title}, author:{item.author}, finished at{" "} */}
+            </ListItem>
+          ))}
+        </List>
+        <Button color="primary">
+          <Link to="/">Back</Link>
+        </Button>
+      </>
     );
   return <>{item}</>;
 };
